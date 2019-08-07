c727de8ea88a621df52336d8eac4cab6a80b53a2
Contrail <30793677+StellaContrail@users.noreply.github.com>
2019-08-07 13:32:45 +0900

    Fixed Rungekutta problem
    Fixed a problem where results divergent because of k's of Rungekutta were not properly set.

--- Harmonic_TimeEvolution_Wavefunctions/program.f90
+++ Harmonic_TimeEvolution_Wavefunctions/program.f90
@@ -1,86 +1,104 @@
 module extensions
     implicit none
-contains
+  contains
+    ! Multiply imaginary unit to an arbitrary complex z
+    ! i(a+ib) = -b + ia = -(imag) + i(real)
+    double complex function ix(z)
+      double complex, intent(in) :: z
+      ix = dcmplx(-imag(z), dble(z))
+    end function ix
+
+    ! Initialize given wave function array
+    ! phi : wave function
+    ! n : dimension of phi
+    ! xl, xu : lower and upper bound for range of x
+    ! x0 : expected value (arbitrary)
+    ! a : inverse of standard deviation
+    ! dh : step of x
     subroutine initialize(phi, n, xl, xu, x0, a, dh)
-        double complex, intent(out) :: phi(0:, 0:)
-        double precision, intent(in) :: xl, xu, x0, a, dh
-        integer, intent(in) :: n
-        integer i
-        double precision x
-        x = 0d0
-        ! Initialize wave functions with zero
-        phi = (0d0, 0d0)
-        ! Initialize at t=0 wave function as Gaussian Wave packet
-        do i = 0, n
-            x = xl + dh * i
-            phi(i, 0) = dcmplx(exp(-0.5d0*a*a*(x-x0)**2d0), 0d0)
-        end do
-        call normalize(phi(:, 0), n, dh)
-        ! Set Boundary Condition
-        phi(0, :) = (0d0, 0d0)
-        phi(n, :) = (0d0, 0d0)
-    end subroutine
+      double complex, intent(out) :: phi(0:, 0:)
+      double precision, intent(in) :: xl, xu, x0, a, dh
+      integer, intent(in) :: n
+      integer i
+      double precision x
+      x = 0d0
+      ! Reset all wave function data
+      phi = (0d0, 0d0)
 
-    subroutine normalize(phi, n, dh)
-        integer,intent(in) :: n
-        double complex,intent(inout) :: phi(:)
-        double precision,intent(in) :: dh
-        double precision sum
-        integer i
-        sum = 0d0
-        do i = 0, n
-            if (i /= 0 .or. i /= n) then
-                sum = sum + norm_2(phi(i))*dh
-            else
-                sum = sum + 0.5d0 * norm_2(phi(i))*dh
-            end if
-        end do
-        phi(:) = phi(:) / sqrt(sum)
-    end subroutine  
+      ! Initialize the wave function at t=0 with initialize condition
+      do i = 0, n
+         x = xl + dh * i
+         phi(i, 0) = dcmplx(exp(-0.5d0*a*a*(x-x0)**2d0), 0d0)
+      end do
 
+      ! Apply boundary condition at x=xl, x=xu
+      phi(0, :) = (0d0, 0d0)
+      phi(n, :) = (0d0, 0d0)
+    end subroutine initialize
+
+    ! Solve Schroedinger equation
+    ! phi : wave function
+    ! n : dimension of the wave function
+    ! xl, xu : lower and upper bound of range x
+    ! dh : step of space
+    ! dt : step of time
     subroutine solve_schroedinger(phi, n, m, xl, xu, dh, dt)
-        double complex, intent(inout) :: phi(0:, 0:)
+      double complex, intent(inout) :: phi(0:, 0:)
         double precision, intent(in) :: xl, xu, dh, dt
         integer, intent(in) :: n, m
         double complex k1(0:n), k2(0:n), k3(0:n), k4(0:n)
         double precision x
         integer i, j
-
-        ! j : Time
+        
         do j = 0, m-1
-            ! i : Coordinates
-            do i = 1, n-1
-                x = xl + i * dh
-                k1(i) = -conjg(H(phi(i-1, j), phi(i, j), phi(i+1,j), x, dh))*dt
-            end do
-            k1(0) = (0d0, 0d0)
-            k1(n) = (0d0, 0d0)
 
-            do i = 1, n-1
-                x = xl + i * dh
-                k2(i) = -conjg(H(phi(i-1, j)+0.5d0*k1(i-1)*dt, phi(i, j)+0.5d0*k1(i)*dt, phi(i+1,j)+0.5d0*k1(i+1)*dt, x, dh))*dt
-            end do
-            k2(0) = (0d0, 0d0)
-            k2(n) = (0d0, 0d0)
-
-            do i = 1, n-1
-                x = xl + i * dh
-                k3(i) = -conjg(H(phi(i-1, j)+0.5d0*k2(i-1)*dt, phi(i, j)+0.5d0*k2(i)*dt, phi(i+1,j)+0.5d0*k2(i+1)*dt, x, dh))*dt
-            end do
-            k3(0) = (0d0, 0d0)
-            k3(n) = (0d0, 0d0)
+           ! when i=0, i=n (which are the indices of both ends'), wave functions are always zero
+           ! so no need to implementation for the calculation
+           ! however, when solving for phi(1), we need information of k(1) which means we are short of k(0)
+           ! this causes a problem because when we solve for k(0), we need k(-1) and so on...
+           do i = 1, n-1
+              x = xl + i * dh
+              k1(i) = -ix(H(phi(i-1, j), phi(i, j), phi(i+1,j), x, dh))*dt
+           end do
+           
+           do i = 1, n-1
+              x = xl + i * dh
+              k2(1) = -ix(H(phi(0, j), phi(1, j)+0.5d0*k1(1), phi(2, j)+0.5d0*k1(2), x, dh))*dt
+              k2(n-1) = -ix(H(phi(n-2, j)+0.5d0*k1(n-2), phi(n-1, j)+0.5d0*k1(n-1), phi(n,j), x, dh))*dt
+              if (1 < i .and. i < n-1) then
+                 k2(i) = -ix(H(phi(i-1, j)+0.5d0*k1(i-1), phi(i, j)+0.5d0*k1(i), phi(i+1,j)+0.5d0*k1(i+1), x, dh))*dt
+              end if
+           end do
             
-            do i = 1, n-1
-                x = xl + i * dh
-                k4(i) = -conjg(H(phi(i-1, j)+k3(i-1)*dt, phi(i, j)+k3(i)*dt, phi(i+1,j)+k3(i+1)*dt, x, dh))*dt
+           do i = 1, n-1
+              x = xl + i * dh
+              k3(1) = -ix(H(phi(0, j), phi(1, j)+0.5d0*k2(1), phi(2, j)+0.5d0*k2(2), x, dh))*dt
+              k3(n-1) = -ix(H(phi(n-2, j)+0.5d0*k2(n-2), phi(n-1, j)+0.5d0*k2(n-1), phi(n,j), x, dh))*dt
+              if (1 < i .and. i < n-1) then
+                 k3(i) = -ix(H(phi(i-1, j)+0.5d0*k2(i-1), phi(i, j)+0.5d0*k2(i), phi(i+1,j)+0.5d0*k2(i+1), x, dh))*dt
+              end if
+           end do
+           
+           do i = 1, n-1
+              x = xl + i * dh
+              k4(1) = -ix(H(phi(0, j), phi(1, j)+k3(1), phi(2, j)+k3(2), x, dh))*dt
+              k4(n-1) = -ix(H(phi(n-2, j)+k3(n-2), phi(n-1, j)+k3(n-1), phi(n,j), x, dh))*dt
+              if (1 < i .and. i < n-1) then
+                 k4(i) = -ix(H(phi(i-1, j)+k3(i-1), phi(i, j)+k3(i), phi(i+1,j)+k3(i+1), x, dh))*dt
+              end if             
             end do
-
+           
             do i = 1, n-1
-                phi(i, j+1) = phi(i, j) + (k1(i) + 2d0*k2(i) + 2d0*k3(i) + k4(i)) / 6d0
+               phi(i, j+1) = phi(i, j) + (k1(i) + 2d0*k2(i) + 2d0*k3(i) + k4(i)) / 6d0
             end do
+            
         end do
     end subroutine
 
+    ! Define hamiltonian when V = 0.5*x*x
+    ! phi_back, phi_center, phi_forward : wave function placed on different positions (back, center, forward)
+    ! x : position of center wave function
+    ! dh : step of space
     double complex function H(phi_back, phi_center, phi_forward, x, dh)
         double complex,intent(in) :: phi_back, phi_center, phi_forward
         double precision,intent(in) :: dh, x
@@ -91,32 +109,87 @@ contains
         H = dcmplx(re, im)
     end function
 
-    subroutine total_probability(phi, prob, dh, n, m)
-        double complex,intent(in) :: phi(0:, 0:)
-        double precision,intent(in) :: dh
-        double precision,intent(out) :: prob(0:)
-        integer,intent(in) ::n,  m
-        double precision sum
-        integer i, j
+    ! Normalization of wave function
+    ! phi : wave function
+    ! dh : step of space
+    ! n : dimension of space
+    subroutine normalize(phi, dh, n)
+      double complex,intent(inout) :: phi(0:, 0:)
+      double precision,intent(in) :: dh
+      integer,intent(in) :: n
 
-        do j = 0, m
-            sum = 0d0
-            do i = 0, n
-                if (i /= 0 .or. i /= n) then
-                    sum = sum + norm_2(phi(i, j)) * dh
-                else
-                    sum = sum + 0.5d0 * norm_2(phi(i, j)) * dh
-                end if
-            end do
-            prob(j) = sum
-        end do
-    end subroutine
+      phi(:, :) = phi(:, :) / sqrt(summation(phi, dh, n, 0))
+    end subroutine normalize
 
-    double precision function norm_2(cmp)
-        double complex,intent(in) :: cmp
-        norm_2 = dble(cmp) * dble(cmp) + imag(cmp) * imag(cmp)
-    end function    
+    double precision function summation(phi, dh, n, j)
+      double complex, intent(in) :: phi(0:, 0:)
+      double precision,intent(in) :: dh
+      integer,intent(in) :: n, j
+      double precision sum
+      integer i
 
+      sum = 0d0
+      do i = 0, n
+         if (i /= 0 .or. i /= n) then
+            sum = sum + abs(phi(i, j))**2d0 * dh
+         else
+            sum = sum + 0.5d0 * abs(phi(i, j))**2d0 * dh
+         end if
+      end do
+      summation = sum
+    end function summation
+
+    subroutine average_x(phi, dh, n, m, xl, avg_x)
+      double complex, intent(in) :: phi(0:, 0:)
+      double precision, intent(in) :: dh, xl
+      double precision, intent(out) :: avg_x(0:)
+      integer,intent(in) :: n, m
+      double precision sum, x
+      integer i, j
+
+      do j = 0, m
+         sum = 0d0
+         do i = 0, n
+            x = xl + dh * i
+            if (i /= 0 .or. i /= n) then
+               sum = sum + abs(phi(i, j))**2d0 * x * x * dh
+            else
+               sum = sum + 0.5d0 * abs(phi(i, j))**2d0 * x * x * dh
+            end if
+         end do
+         avg_x(j) = sum
+      end do
+    end subroutine average_x
+
+    subroutine average_p(phi, dh, dt, n, m, xl, avg_p)
+      double complex, intent(in) :: phi(0:, 0:)
+      double complex sum
+      double precision, intent(in) :: dh, dt, xl
+      double complex, intent(out) :: avg_p(0:)
+      double precision x
+      integer, intent(in) :: n, m
+      integer i, j
+
+      do j = 0, m
+         sum = dcmplx(0d0, 0d0)
+         do i = 0, n
+            x = xl + dh * i
+            if (i /= 0 .or. i /= n) then
+                sum = sum + conjg(phi(i, j))*H(phi(i-1,j), phi(i,j),phi(i+1,j),x,dh) * dh / dt
+            else
+               sum = sum + 0.5d0 * conjg(phi(i, j))*H(phi(i-1,j), phi(i,j), phi(i+1,j), x, dh) * dh/dt
+            end if
+         end do
+         avg_p(j) = -ix(sum)
+      end do
+    end subroutine average_p
+    
+    ! Output wave function
+    ! mode : output mode
+    ! i, j : index of space and time respectively
+    ! xl, xu : lower and upper bound of range x
+    ! n, m : dimensions of space and time respectively
+    ! dh, dt : step of space and time respectively
     subroutine output(phi, mode, i, j, xl, xu, n, m, dh, dt)
         double complex,intent(in) :: phi(0:, 0:)
         double precision,intent(in) :: xl, xu, dt, dh
@@ -140,7 +213,7 @@ contains
             do k = 0, m
                 time = dt * k
                 do l = 0, n
-                    write (fo, *) time, xl + dh*l, dble(phi(l, k)), imag(phi(l, k))
+                   write (fo, *) time, xl + dh*l, dble(phi(l, k)), imag(phi(l, k)), abs(phi(l, k))**2d0
                 end do
                 write (fo, *)
             end do
@@ -156,14 +229,27 @@ end module
 program main
     use extensions
     implicit none
-    integer,parameter          :: n  = 200, m  = 2000
-    double precision,parameter :: dh = 0.2d0, dt = 0.01d0
-    double precision :: xl = -20d0, xu = 20d0, x0 = 0d0, a = 0.5d0, prob(0:m)
-    double complex :: phi(0:n, 0:m) = (0d0, 0d0)
+    integer,parameter :: n = 250, m = 4000
+    double precision :: xl = -10d0, xu = 20d0, x0 = 0d0, a = 1d0, prob(0:m)
+    double precision :: avg_x(0:m)
+    double complex :: phi(0:n, 0:m) = (0d0, 0d0), avg_p(0:m) = (0d0, 0d0)
+    double precision,parameter :: dh = 0.08d0, dt = 0.0016d0
 
     call initialize(phi, n, xl, xu, x0, a, dh)
     call solve_schroedinger(phi, n, m, xl, xu, dh, dt)
+    call normalize(phi, dh, n)
     call output(phi, "A", 0, 0, xl, xu, n, m, dh, dt)
-    call total_probability(phi, prob, dh, n, m)
-    write (*, *) prob(0), prob(1), prob(10), prob(100), prob(1000), prob(2000)
+    write (*, *)
+    write (*, *) "Summation of Wave function"
+    write (*, *) summation(phi, dh, n, 0)
+    write (*, *) summation(phi, dh, n, 100)
+    write (*, *) summation(phi, dh, n, 1000)
+    call average_x(phi, dh, n, m, xl, avg_x)
+    call average_p(phi, dh, dt, n, m, xl, avg_p)
+    write (*, *) "Expected values of x"
+    write (*, *) "j=0   :", avg_x(0)
+    write (*, *) "j=100 :", avg_x(100)
+    write (*, *) "j=1000:", avg_x(1000)
+    write (*, *) "Expected value of p   real imag"
+    write (*, *) "j=0   :", dble(avg_p(0)), imag(avg_p(0))
 end program
