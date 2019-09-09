ad492c7e930a7647847e2b0d25d8b4a5405b0a23
StellaContrail <sky.contrail.man+github@gmail.com>
2019-09-09 21:59:19 +0900

    Added expected energy value

--- Harmonic_TimeEvolution_Wavefunctions/FDM/program.f90
+++ Harmonic_TimeEvolution_Wavefunctions/FDM/program.f90
@@ -1,19 +1,20 @@
 module extensions
     implicit none
-    double precision,parameter :: ALPHA = 1d0 ! (HBAR*C)^2/(2*MC^2)
-    double precision,parameter :: BETA = 0.5d0 ! MASS*OMEGA^2
     double precision,parameter :: HBAR = 1d0
+    double precision,parameter :: MASS = 0.5d0
+    double precision,parameter :: OMEGA = 1d0
     double precision,parameter :: K = 0d0 ! wave number
-    double precision,parameter :: a = 0.5d0
+    double precision,parameter :: a = 2d0
+    double precision,parameter :: x0 = 1.5d0 ! initial center point of Gaussian wave packet
 contains
     ! i*(a+ib)=-b+ia : (REAL=-b), (IMAG=a)
-    function ix(A, n)
+    function ix(A_matrix, n)
         integer,intent(in) :: n
-        double complex,intent(in) :: A(1:n)
+        double complex,intent(in) :: A_matrix(1:n)
         double complex ix(1:n)
         integer i
         do i = 1, n
-            ix(i) = dcmplx(-aimag(A(i)), dble(A(i)))
+            ix(i) = dcmplx(-aimag(A_matrix(i)), dble(A_matrix(i)))
         end do
     end function
     function ix_scaler(z)
@@ -22,11 +23,11 @@ contains
         ix_scaler = dcmplx(-aimag(z), dble(z))
     end function
 
-    subroutine solve_schroedinger(phi, n, m, dh, dt, xl)
+    subroutine solve_schroedinger(phi, n, m, dh, dt, xl, H)
         integer,intent(in) :: n, m
         double complex,intent(inout) :: phi(1:n, 1:m)
         double precision,intent(in) :: dh, dt, xl
-        double precision H(1:n, 1:n)
+        double precision,intent(out) ::  H(1:n, 1:n)
         double precision t1, t2, speed
         integer i
         H = 0d0
@@ -34,7 +35,7 @@ contains
 
         phi = dcmplx(0d0, 0d0)
         call initialize(phi(:, 1), n, dh, xl)
-        call normalize(phi(:, 1), n, dh)
+        call normalize(phi(:, 1), n)
 
         call cpu_time(t1)
         do i = 1, m-1
@@ -50,30 +51,22 @@ contains
         end do
     end subroutine
 
-    subroutine normalize(phi, n, dh)
+    subroutine normalize(phi, n)
         integer,intent(in) :: n
         double complex,intent(inout) :: phi(1:n)
-        double precision,intent(in) :: dh
-
-        phi = phi / sqrt(calc_probability(phi, n, dh))
+        double complex prob
+        prob = calc_probability(phi, n)
+        if (aimag(prob) > 1d-15) then
+            stop "Probability calculation failed"
+        end if
+        phi = phi / sqrt(prob)
     end subroutine
 
-    double precision function calc_probability(phi, n, dh)
+    ! Calculate total probability of the system
+    double complex function calc_probability(phi, n)
         integer,intent(in) :: n
         double complex,intent(in) :: phi(1:n)
-        double precision,intent(in) :: dh
-        double precision sum
-        integer i
-
-        sum = 0d0
-        do i = 1, n
-            if (i == 1 .or. i == n) then
-                sum = sum + 0.5d0*abs(phi(i))**2d0*dh
-            else
-                sum = sum + abs(phi(i))**2d0*dh
-            end if
-        end do
-        calc_probability = sum
+        calc_probability = dot_product(phi, phi)
     end function
 
     subroutine initialize(phi, n, dh, xl)
@@ -85,7 +78,7 @@ contains
 
         do i = 1, n
             x = xl + dh * i
-            phi(i) = dcmplx(cos(K*x)*exp(-0.5d0*x**2d0/a), sin(K*x)*exp(-0.5d0*x**2d0/a))
+            phi(i) = dcmplx(cos(K*x)*exp(-0.5d0*(x-x0)**2d0/a), sin(K*x)*exp(-0.5d0*(x-x0)**2d0/a))
         end do  
     end subroutine  
 
@@ -118,6 +111,7 @@ contains
     end function
 
     subroutine construct_hamiltonian(H, n, xl, dh)
+        double precision,parameter :: ALPHA = 0.5d0*HBAR*HBAR/MASS ! Coefficient of Kinetic Energy
         integer,intent(in) :: n
         double precision,intent(out) :: H(1:n, 1:n)
         double precision,intent(in)  :: xl, dh
@@ -149,6 +143,7 @@ contains
         end do
     end subroutine
     double precision function V(x)
+        double precision,parameter :: BETA = MASS * OMEGA * OMEGA
         double precision,optional,intent(in) :: x
         V = 0.5d0*x*x*BETA
     end function
@@ -177,7 +172,7 @@ contains
         integer,intent(in) :: n, m
         double precision,intent(in) :: dh, xl
         double complex,intent(in) :: phi(1:n, 1:m)
-        double precision solve_x(1:m)
+        double complex solve_x(1:m)
         double precision x
         integer i, j
         solve_x = 0d0
@@ -185,52 +180,67 @@ contains
         do j = 1, m
             do i = 1, n
                 x = xl + dh * i
-                if (i == 1 .or. i == n) then
-                    solve_x(j) = solve_x(j) + 0.5d0*x*abs(phi(i, j))**2d0*dh
-                else
-                    solve_x(j) = solve_x(j) + x*abs(phi(i, j))**2d0*dh
-                end if
+                solve_x(j) = solve_x(j) + x * phi(i, j)**2d0
             end do
         end do
 
     end function
 
-    function solve_p(phi, n, m)
+    function solve_p(phi, n, m, dh)
         integer,intent(in) :: n, m
         double complex,intent(in) :: phi(1:n, 1:m)
-        double complex solve_p(1:m)
+        double complex solve_p(1:m), temp(1:n)
+        double precision,intent(in) :: dh
+        double precision diff_op(1:n, 1:n)
         integer i, j
         solve_p = dcmplx(0d0, 0d0)
 
+        diff_op = 0d0
+        diff_op(1,1) = -1d0
+        diff_op(1,2) = 1d0
+        diff_op(n,n-1) = -1d0
+        diff_op(n,n) = 1d0
+        do i = 2, n-1
+            diff_op(i, i+1) = 0.5d0
+            diff_op(i, i-1) = -0.5d0
+        end do
+        diff_op = diff_op / dh
+
         do j = 1, m
-            do i = 1, n
-                if (i == 1) then
-                    solve_p(j) = solve_p(j) - 0.5d0*HBAR*conjg(phi(1,j))*ix_scaler(phi(2,j)-phi(1,j))
-                else if (i == n) then
-                    solve_p(j) = solve_p(j) - 0.5d0*HBAR*conjg(phi(n,j))*ix_scaler(phi(n,j)-phi(n-1,j))
-                else
-                    solve_p(j) = solve_p(j) - HBAR*conjg(phi(i,j))*ix_scaler(0.5d0*(phi(i+1,j)-phi(i-1,j)))
-                end if
-            end do
+            temp = matmul(diff_op, phi(:, j))
+            solve_p(j) = -HBAR*ix_scaler(dot_product(phi(:, j), temp))
         end do
 
     end function
+
+    subroutine solve_energy(phi, n, m, H, energies)
+        integer,intent(in) :: n, m
+        double complex,intent(in) :: phi(1:n, 1:m)
+        double precision,intent(in) :: H(1:n, 1:n)
+        double complex,intent(out) :: energies(1:m)
+        double complex :: temp(1:n, 1:m)
+        integer j
+        do j = 1, m
+            temp(:, j) = matmul(H, phi(:, j))
+            energies(j) = dot_product(phi(:, j), temp(:, j))
+        end do
+    end subroutine
 end module
 
 program main
     use extensions
     implicit none
-    integer,parameter :: n = 150, m = 630+1
+    integer,parameter :: n = 150, m = 630+1, m_display_step = 100
     double precision,parameter :: dh = 0.2d0, dt = 0.02d0, xl = -dh*(n/2)
     double precision t1, t2
-    double complex phi(1:n, 1:m), p(1:m)
-    double precision x(1:m)
+    double complex phi(1:n, 1:m), p(1:m), E(1:m), x(1:m)
+    double precision H(1:n, 1:n)
     integer j
     phi = dcmplx(0d0, 0d0)
 
     write (*, *) "Start Calculation..."
     call cpu_time(t1)
-    call solve_schroedinger(phi, n, m, dh, dt, xl)
+    call solve_schroedinger(phi, n, m, dh, dt, xl, H)
     call cpu_time(t2)
     write (*, *) t2 - t1, " seconds"
 
@@ -238,19 +248,25 @@ program main
     call plot(phi, n, m, dh, xl)
 
     write (*, *) "Time dependance of probability"
-    do j = 1, m, 1000
-        write (*, *) "j=", j, calc_probability(phi(:,j), n, dh)
+    do j = 1, m, m_display_step
+        write (*, *) "j=", j, dble(calc_probability(phi(:,j), n))
     end do
 
     write (*, *) "Time dependance of expected value of x"
     x = solve_x(phi, n, m, dh, xl)
-    do j = 1, m, 1000
-        write (*, '(A, I10, A, F15.10)') "j=", j, " X=", x(j)
+    do j = 1, m, m_display_step
+        write (*, '(A, I10, 2(A, F15.10))') "j=", j, " REAL=", dble(x(j)), " IMAG=", aimag(x(j))
     end do
 
     write (*, *) "Time dependance of expected value of p"
-    p = solve_p(phi, n, m)
-    do j = 1, m, 1000
+    p = solve_p(phi, n, m, dh)
+    do j = 1, m, m_display_step
         write (*, '(A, I10, 2(A, F15.10))') "j=", j, " REAL=", dble(p(j)), " IMAG=", aimag(p(j))
+    end do
+
+    write (*, *) "Time dependance of expected energy E"
+    call solve_energy(phi, n, m, H, E)
+    do j = 1, m, m_display_step
+        write (*, '(A, I10, 2(A, F15.10))') "j=", j, " REAL=", dble(E(j)), " IMAG=", aimag(E(j))
     end do
 end program 
