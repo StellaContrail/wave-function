bc0a50d1ef12f77fc3e1571a5afdb4c0c48a5065
StellaContrail <sky.contrail.man+github@gmail.com>
2019-10-19 05:09:04 +0900

    Added some detailed output system

--- Harmonic_TimeEvolution_Wavefunctions/three_dimension/program.f90
+++ Harmonic_TimeEvolution_Wavefunctions/three_dimension/program.f90
@@ -4,35 +4,67 @@ module extension
     double precision,parameter :: HBAR = 1d0    ! Plank constant
     double precision,parameter :: MASS = 0.5d0  ! mass of the harmonic oscillator
     double precision,parameter :: OMEGA = 1d0   ! angular frequency of the potential
+    double precision,parameter :: x_zero(3) = (/5.5d0, 5.5d0, 5.5d0/)
+    double precision,parameter :: k_zero(3) = (/-1d0, -1d0, -1d0/)
     ! squared covariance of initial Gaussian wavepacket
     double precision,parameter :: a = HBAR / (MASS * OMEGA)
 contains
+    function V(x, y, z) result(potential)
+        double precision,intent(in) :: x, y, z
+        double precision potential
+        double precision,parameter :: BETA = MASS * OMEGA * OMEGA
+        double precision r2, r
+        r2 = x*x+y*y+z*z
+        r = sqrt(r2)
+        potential = 0.5d0*r2*BETA
+        
+        !potential = -5d0*exp(-r2)
+        
+        !if (abs(x) < 5d0 .and. abs(y) < 15d0 .and. abs(z) < 15d0) then
+        !    potential = -4d0
+        !else
+        !    potential = 0d0
+        !end if
+        
+        if (2d0 < r .and. r < 5d0) then
+            potential = -50d0
+        else 
+            potential = 0d0
+        end if
+        
+        !potential = 0d0
+    end function
     subroutine solve(n, m, dh, dt, xl)
         integer,intent(in) :: n, m
         double precision,intent(in) :: dh, dt, xl
         double complex phi(1:n, 1:n, 1:n)
         integer i
+        write (*, *) "Initializing wave function"
         call initialize(phi, n, dh, xl)
+        write (*, *) "Initialized wave function"
+        write (*, *) "Calculating time developed wave function"
         !call normalize(phi, n, dh)
         open(10, file="data.txt")
         call plot(phi, n, dh, xl, 10)
-        write (*, '(A, I5)') "i=", 0
-        write (*, '(A, F15.10)') "P=", probability(phi, n, dh)
-        write (*, '(A, 2F15.10)') "E=", energy(phi, n, dh, xl)
-        write (*, '(A, 2F15.10)') "x=", expect_x(phi, n, dh, xl)
+        write (*, '(A, I5, A, F4.1, A)') "Calculating at the time step of i = ", 0, " ( t = ", dt*0, " seconds )"
+        write (*, '(A, F15.10)')  "Probability      = ", probability(phi, n, dh)
+        write (*, '(A, 2F15.10)') "Energy           = ", energy(phi, n, dh, xl)
+        write (*, '(A, 2F15.10)') "x expected value = ", expect_x(phi, n, dh, xl)
         write (*, *)
 
         do i = 1, m
             phi = evolve(phi, n, dh, dt, xl)
             if (mod(i, 25) == 0d0) then
                 call plot(phi, n, dh, xl, 10)
-                write (*, '(A, I5)') "i=", i
-                write (*, '(A, F15.10)') "P=", probability(phi, n, dh)
-                write (*, '(A, 2F15.10)') "E=", energy(phi, n, dh, xl)
-                write (*, '(A, 2F15.10)') "x=", expect_x(phi, n, dh, xl)
+                write (*, '(A, I5, A, F4.1, A)') "Calculating at the time step of i = ", i, " ( t = ", dt*i, " seconds )"
+                write (*, '(A, F15.10)')  "Probability      = ", probability(phi, n, dh)
+                write (*, '(A, 2F15.10)') "Energy           = ", energy(phi, n, dh, xl)
+                write (*, '(A, 2F15.10)') "x expected value = ", expect_x(phi, n, dh, xl)
                 write (*, *)
             end if
         end do
+        write (*, *) "Calculated wave function of all time steps"
+        write (*, *) "Each calculated wave function is stored in data.txt"
         close(10)
     end subroutine
     subroutine initialize(phi, n, dh, xl)
@@ -40,18 +72,20 @@ contains
         double complex,intent(out) :: phi(1:n, 1:n, 1:n)
         double precision,intent(in) :: dh, xl
         integer i, j, k
-        double precision x, y, z
+        double precision x, y, z, phase
 
         do k = 1, n
             z = xl + dh * k
-            z = z - 2.3d0
+            z = z - x_zero(3)
             do j = 1, n
                 y = xl + dh * j
-                y = y - 2.3d0
+                y = y - x_zero(2)
                 do i = 1, n
                     x = xl + dh * i
-                    x = x - 2.3d0
+                    x = x - x_zero(1)
                     phi(i, j, k) = (a*acos(-1d0))**(-0.75d0)*exp(-0.5d0*(x*x+y*y+z*z)/a)
+                    phase = k_zero(1)*x+k_zero(2)*y+k_zero(3)*z
+                    phi(i, j, k) = phi(i, j, k) * dcmplx(cos(phase), sin(phase))
                 end do
             end do
         end do
@@ -101,7 +135,7 @@ contains
         double precision,intent(in) :: dh, xl
         double complex,intent(in) :: phi(1:n, 1:n, 1:n)
         integer i, j, k
-        double precision x, y, z
+        double precision x, y
         double complex density(1:n, 1:n)
         do j = 1, n
             do k = 1, n
@@ -204,27 +238,6 @@ contains
                 end do
             end do
         end do
-    end function
-    function V(x, y, z) result(potential)
-        double precision,intent(in) :: x, y, z
-        double precision potential
-        double precision,parameter :: BETA = MASS * OMEGA * OMEGA
-        double precision r2, r
-        r2 = x*x+y*y+z*z
-        r = sqrt(r2)
-        !potential = 0.5d0*r2*BETA
-        !potential = -5d0*exp(-r2)
-        !if (abs(x) < 5d0 .and. abs(y) < 5d0 .and. abs(z) < 5d0) then
-        !    potential = -50d0
-        !else
-        !    potential = 0d0
-        !end if
-        if (2d0 < r .and. r < 6d0) then
-            potential = -50d0
-        else 
-            potential = 0d0
-        end if
-        !potential = 0d0
     end function
     function ix(matrix, n) result(output_matrix)
         integer,intent(in) :: n
@@ -336,9 +349,21 @@ program main
     double precision,parameter :: xl = -dh*(n/2)
     double precision t1, t2
 
+    write (*, *) "Constants"
+    write (*, '(A, I4)') "Space Size :  n = ", n
+    write (*, '(A, I5)') "Time Size  :  m = ", m
+    write (*, '(A, F5.3)') "Space Step : dh = ", dh
+    write (*, '(A, F5.3)') "Time Step  : dt = ", dt
+    write (*, '(A, F5.3)') "Plank constant : hbar = ", HBAR
+    write (*, '(A, F5.3)') "Mass : MASS = ", MASS
+    write (*, '(A, F5.3)') "Angular velocity : omega = ", OMEGA
+    write (*, '(A, 3(F6.3, A))') "Initial Center Position : (x, y, z) = (", x_zero(1), ", ", x_zero(2), ", ", x_zero(3), ")"
+    write (*, '(A, 3(F6.3, A))') "Initial Wave Number : (kx, ky, kz) = (", k_zero(1), ", ", k_zero(2), ", ", k_zero(3), ")"
+    write (*, *)
+
     write (*, *) "Start Calculation..."
     call cpu_time(t1)
     call solve(n, m, dh, dt, xl)
     call cpu_time(t2)
-    write (*, '(A, F15.10, A)') "Calculation Time : ", t2 - t1, " seconds"
+    write (*, '(A, F5.1, A)') "Calculation Time : ", t2 - t1, " seconds"
 end program
