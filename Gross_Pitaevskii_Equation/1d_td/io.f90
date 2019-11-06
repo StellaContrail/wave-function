865cf034c07de85119e8908fdf8b0db5458671e1
StellaContrail <sky.contrail.man+github@gmail.com>
2019-11-07 00:21:27 +0900

    Resolved confliction

--- Gross_Pitaevskii_Equation/1d_td/io.f90
+++ Gross_Pitaevskii_Equation/1d_td/io.f90
@@ -10,11 +10,7 @@ contains
     integer i
     do i = 1, N
        x = -xmax + dh * i
-<<<<<<< HEAD
        write (unit, '(*(F15.5, X))') x, real(f(i)), aimag(f(i)), abs(f(i))**2d0
-=======
-       write (unit, '(F15.5, X, F15.5)') x, abs(f(i))**2d0
->>>>>>> 3096838a4ae31bf9300dfcf01b45c983364a4c68
     end do
     write (unit, *)
   end subroutine output
