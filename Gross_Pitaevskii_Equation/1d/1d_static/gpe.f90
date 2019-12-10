7986793f0a7e1b9a89eda1187e45b78786880a5e
Contrail <contrail@ubuntu.ubuntu-domain>
2019-12-10 17:02:45 +0900

    Added non phased axially symmetric HO potential

--- Gross_Pitaevskii_Equation/1d/1d_static/gpe.f90
+++ Gross_Pitaevskii_Equation/1d/1d_static/gpe.f90
@@ -11,13 +11,12 @@ program main
     ! Physical values
     integer                        :: N                ! Number of division in space
     integer                        :: DIM              ! Dimension of discretized wave function array
-    double precision,allocatable   :: Phi_temp(:)      ! Temporary wave function used by zhbev
+    double precision,allocatable   :: Phi_temp(:)      ! Temporary wave function
     double precision,allocatable   :: Phi_next(:)      ! Wave function at next step
     double precision,allocatable   :: Phi_prev(:)      ! Wave function at previous step
     double precision,allocatable   :: Pot(:)           ! Potential
     double precision,allocatable   :: Pot_TD(:)        ! Time-Dependent Potential
     double precision,allocatable   :: H(:,:)           ! Hamiltonian
-    double precision,allocatable   :: H_temp(:,:)      ! Temporary Hamiltonian used by zhbev
     double precision               :: dh               ! Step of distance in the x-direction
     double precision               :: xmax             ! largest x position (Boundary position)
     double precision               :: mass             ! mass of a boson
@@ -30,9 +29,8 @@ program main
     double precision               :: Xs               ! characteristic length of the condensate
     double precision               :: epsilon          ! squared ratio of Azero to Xs
     double precision               :: kappa            ! coeffient of the nonlinear term
-    double precision               :: mu0              ! chemical potential returned by zhbev
+    double precision               :: mu_old           ! chemical potential returned by zhbev
     integer                        :: i,j,k            ! Loop variable
-    logical                        :: loop_end_flag    ! Loop end flag
     double precision               :: Degree           ! Degree of asymmetry of wave function
     ! Definition of physical values (this could be replaced with I/O)
     ! These values are referenced from
@@ -42,23 +40,23 @@ program main
     omega            = 20d0 * pi
     ParticleCount    = 1000 ! Until 120 or so is the threshold for precise calculation
     ScatteringLength = 5.1d-9
-    !N                = 2**4 - 1!129   ! N must be an odd number
+    N                = 2**7 - 1!256   ! N must be an odd number
     ! Calculation of coefficients and variables using defined physical values
-    xmax    = 5d0
+    xmax    = 15d0
     Azero   = sqrt(hbar/(omega*mass))
-    Xs      = 2d0*Azero   ! Usually chosen to be Azero for a weak/moderate interaction
+    Xs      = Azero
     epsilon = (Azero/Xs)**2d0
     kappa   = (4d0*pi*ScatteringLength*ParticleCount/Azero)*(Azero/Xs)**5d0
-    !dh      = xmax / (n/2 + 0.5d0)
-    dh      = 0.8d0
+    dh      = xmax / (n/2 + 0.5d0)
+    !dh      = 0.8d0
     !xmax    = (N+0.5d0)*dh
-    N       = int(2d0*xmax/dh)
-    if (mod(N, 2) == 0) then
-        N = N + 1
-    end if
+    !N       = int(2d0*xmax/dh)
+    !if (mod(N, 2) == 0) then
+    !    N = N + 1
+    !end if
     DIM              = N + 1 ! Include n=0 point
     allocate (Phi_next(0:N), Phi_prev(0:N), Pot(0:N), H(0:N,0:N), Pot_TD(0:N))
-    allocate (Phi_temp(1:DIM), H_temp(1:DIM,1:DIM))
+    allocate (Phi_temp(0:N))
 
     ! Show configuration of fundamental physical constants
     print *, "Physical constants of the system----------------------------------"
@@ -70,7 +68,7 @@ program main
     print *, "n  (Dimension of the space)   [count] = ", N
     print *, "A0 (Length of the HO Ground State)[m] = ", Azero
     print *, "Xs (Characteristic Length)        [m] = ", Xs
-    print *, "dh (Step of distance)             [m] = ", dh*Xs
+    print *, "dh (Step of distance)                 = ", dh*Xs
     print *, "<Coefficients of NLSE terms>"
     print *, "Epsilon (A0/Xs)^2                     = ", epsilon
     print *, "Kappa (Coefficient of NL term)        = ", kappa
@@ -80,11 +78,13 @@ program main
     print *, "------------------------------------------------------------------"
     write (*, *)
     
+    write (*, *) "Hit Enter to start calculation..."
+    read (*, *)
+
     print *, "Calculation Start-----------------------------------------"
     ! Initialization of wave function and potential
     call initialize(Phi_next, Phi_prev, Pot, N, dh, xmax)
     write (*, *) "- Initialized the wave function and potential function"
-
     Pot_TD(:) = Pot(:)
 
     ! For development usefulness (TO BE DELETED)
@@ -105,50 +105,41 @@ program main
 
     ! Start I/O Procedure
     open(10, file="data.txt")
-    open(30, file="data_td_potential.txt")
+    open(30, file="data_density_shift.txt")
     ! Set the loop exit flag to be false
-    loop_end_flag = .false.
     do i = 1, 1000
         if (mod(i, 50) == 0) then
-            write (*, *) "ITERATION COUNT = ", i
+            write (*, '(A, I0, A)') " * ", i, " calculations have been done"
         end if
 
+        ! Solve first density
         call hamiltonian(H, Pot, abs(Phi_prev)**2d0, N, dh, epsilon, kappa)
-        do j = 0, N
-            do k = 0, N
-                H_temp(j+1, k+1) = H(j,k)
-            end do
-        end do
-
-        Pot_TD(:) =  Pot(:) + kappa*abs(Phi_prev(:))**2d0
-        call output_potential(30, Pot_TD, N, dh, xmax)
-
-        call solve_eigen(H_temp(1:DIM, 1:DIM), Phi_temp(1:DIM), mu0, DIM)
-        do j = 0, N
-            Phi_next(j) = Phi_temp(j+1)
-        end do
+        call solve_eigen(H, Phi_next, mu, DIM)
+        ! Mix densities
+        Phi_temp(:) = sqrt(0.7d0*abs(Phi_prev)**2d0+0.3d0*abs(Phi_next)**2d0)
+        ! Solve using mixed density
+        call hamiltonian(H, Pot, abs(Phi_temp)**2d0, N, dh, epsilon, kappa)
+        call solve_eigen(H, Phi_next, mu, DIM)
+        ! Normalization
         call normalize(Phi_next, N, dh)
-        Phi_prev(0:N) = Phi_next(0:N)
 
         ! For development usefulness : TO BE DELETED
-        call calc_asymmetry_degree(Phi_prev, N, Degree)
+        call calc_asymmetry_degree(Phi_next, N, Degree)
         write (20, *) i, Degree
-        write (20, *)
 
-        if (abs(mu0 - mu) < 1d-6) then
-            loop_end_flag = .true.
-        end if
+        call output_real(30, Phi_next, N, dh, xmax)
 
-        mu = mu0
-
-        if (loop_end_flag) then
+        if (abs(mu_old - mu) < 1d-6) then
             exit
         end if
+        mu_old = mu
+        Phi_prev(0:N) = Phi_next(0:N)
     end do
     close(20)
     write (*, *) "- All calculation procedures have been finished"
     call output_real(10, Phi_prev, N, dh, xmax)
     close (10)
+    close (30)
     call calc_asymmetry_degree(Phi_prev, N, Degree)
     write (*, '(X, A, F15.10)') "- Degree of system's asymmetry = ", Degree
     write (*, *) "- Calculation result has been saved into ", "data.txt"
